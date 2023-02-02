5c69c954be80b3aa8c53e9fcb504b1b306928fc4
Willian Lemann <willianleman@hotmail.com>
2023-02-02 14:19:59 -0300

    feat: width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { SliderFramerMotion } from "../components/SliderFramerMotion";
 export default function Home() {
   return (
     <div className="h-screen w-screen flex justify-center items-center relative">
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-10   h-full items-center justify-center p-4 overflow-hidden">
         <h1>using react-spring</h1>
         <Slider />
 
